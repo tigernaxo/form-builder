fa95198cac253101827a883494aab92a041ebfe9
tigernaxo <tigernaxo.co.cc@gmail.com>
2022-01-15 02:21:52 +0800

    tmp

--- form-builder-app/src/views/Builder.vue
+++ form-builder-app/src/views/Builder.vue
@@ -3,6 +3,7 @@
     <div class="col-2 h-100 bg-primary">
       <div class="container"></div>
         <div draggable="true" 
+        @dragstart="dragstartHandler($event)"
         >
           One of three columns
           </div>
@@ -12,8 +13,9 @@
       <div class="container" style="overflow: auto; height: 100%; background: black;">
         <div id="test" droppable="true" 
         @drop="test($event, 'drop')" 
-        @dragover="test($event, 'dropover')"
-        @dragleave="test($event, 'dragleave')" 
+        @dragover.prevent="dragoverHandler($event)"
+        @dragleave="monk($event, 'dragleave')" 
+        @dragenter.prevent 
         style="background: white"> One of three columns </div>
       </div>
     </div>
@@ -27,8 +29,27 @@
 <script setup lang="ts">
 // To change that behavior so that an element becomes a drop zone or is droppable, 
 // the element must have both ondragover (en-US) and ondrop (en-US) event handler attributes. 
+// https://stackoverflow.com/questions/63063312/why-doesnt-the-drop-event-work-for-me-in-vue
+// In order to make the div a drop target, the div's dragenter and dragover events must be canceled. 
+
+// todo:
+// clonNode
+// record source element information
+// node proccessing(copy, moving... etc) must be done by self
 function test(e: DragEvent, str: string){
-  console.log(str,e.target, e)
+  console.log(str,e.target, e.dataTransfer?.items,e)
+}
+function dragstartHandler(e: DragEvent){
+  let el = <HTMLElement>e.target;
+  e.dataTransfer?.setData("text/plain", el.id);
+  e.dataTransfer!.effectAllowed = "copy";
+
+}
+function dragoverHandler(e: DragEvent){
+  e.dataTransfer!.dropEffect = "copy"
+  console.log(e.dataTransfer?.dropEffect)
+}
+function monk(e: DragEvent, str: string){
 }
 </script>
 
--- form-builder-app/src/views/Temp.vue
+++ form-builder-app/src/views/Temp.vue
@@ -1,11 +1,54 @@
 <template>
-	<div>
-		Temp
-	</div>
+<h1>Examples of <code>DataTransfer</code>.{<code>types</code>, <code>items</code>} properties</h1>
+	<ul>
+		<li id="i1" @dragstart="dragstart_handler($event);" draggable="true">Drag Item 1 to the Drop Zone</li>
+		<li id="i2" @dragstart="dragstart_handler($event);" draggable="true">Drag Item 2 to the Drop Zone</li>
+	</ul>
+	<div id="target" @drop="drop_handler($event);" @dragover="dragover_handler($event);">Drop Zone</div>
 </template>
 
 <script setup lang="ts">
+function dragstart_handler(ev: DragEvent) {
+	console.log("dragStart: target.id = " + (<HTMLElement>ev.target).id);
+	// Add this element's id to the drag payload so the drop handler will
+	// know which element to add to its tree
+	ev.dataTransfer?.setData("text/plain", (<HTMLElement>ev.target).id);
+	ev.dataTransfer!.effectAllowed = "move";
+}
 
+function drop_handler(ev: DragEvent) {
+	console.log("drop: target.id = " + ev.target);
+	ev.preventDefault();
+	// Get the id of the target and add the moved element to the target's DOM
+	var data = ev.dataTransfer?.getData("text");
+	var items = ev.dataTransfer?.items
+	console.log('data', data)
+	console.log('items', items)
+	if(data)
+	{
+		let el = document.getElementById(data)
+		el && (<HTMLElement>ev.target)?.appendChild(el);
+	}
+	// Print each format type
+	if (ev.dataTransfer?.types != null) {
+		for (var i=0; i < ev.dataTransfer.types.length; i++) {
+			console.log("... types[" + i + "] = " + ev.dataTransfer.types[i]);
+		}
+	}
+	// Print each item's "kind" and "type"
+	if (ev.dataTransfer?.items != null) {
+		for (var i=0; i < ev.dataTransfer.items.length; i++) {
+			console.log("... items[" + i + "].kind = " + ev.dataTransfer.items[i].kind + " ; type = " + ev.dataTransfer.items[i].type);
+		}
+	}
+}
+
+function dragover_handler(ev: DragEvent) {
+ console.log("dragOver");
+ ev.preventDefault();
+ // Set the dropEffect to move
+ ev.dataTransfer!.dropEffect="move"
+}
 </script>
 
 <style scoped>
